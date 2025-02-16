bf096727eae7c0018f34c7b62e535f131ee40f03
Habib Ahmad Siddiqui <khalidhabib928@gmail.com>
2025-02-16 15:13:16 +0500

    Add files via upload

---
+++ Check user entered number is even or odd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"Enter a Number to check it is even or odd:";
+	cin>>num;
+	if(num%2==0)
+	{
+		cout<<num <<"is Even";
+	}
+	else
+	{
+		cout<<num <<"is Odd";
+	}
+}
